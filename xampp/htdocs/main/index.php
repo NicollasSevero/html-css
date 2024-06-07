79f818100a968bd25743d772ded0761da06d9e7f
Nicollas Bonfim Severo da Silva <154918468+NicollasSevero@users.noreply.github.com>
2024-06-06 21:02:13 -0300

    update

--- xampp/htdocs/main/index.php
+++ xampp/htdocs/main/index.php
@@ -1,30 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        .principal{
-            color:red;
-        }
-    </style>
+    <title>7A Hotéis</title>
+    <link rel="stylesheet" href="style/style.css">
 
 </head>
 <body>
-    <h1 class>Hello world</h1>
-    <?php 
-        echo  "<h1 class='principal'> hello worlddddd</h1>";
-    ?>
-
-    <?= 'hello woooorld'?>
-    <?php 
-        echo "<button onclick='nicolau()'>Clique aqui</button>";
-    ?>
-    <script>
-        function nicolau(){
-            alert('Bem vindo ao TachWorld')
-        }
-    </script>
+    
 </body>
 </html>
